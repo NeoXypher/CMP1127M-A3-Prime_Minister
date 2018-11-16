db0008a430224d0c14b0a8de048522e1524fb4eb
NeoXypher <Aquiliawtf@gmail.com>
2018-11-16 12:52:56 +0000

    Add project files.

---
+++ CMP1127M A3 Prime_Minister/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1127M_A3_Prime_Minister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
